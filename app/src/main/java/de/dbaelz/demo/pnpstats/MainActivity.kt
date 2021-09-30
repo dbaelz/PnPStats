e83907c61e369270bd5fa0908abe14111c850216
Daniel Bälz <me@dbaelz.de>
2021-09-30 22:05:17 +0200

    Bump up Compose version and target SDK, add Hilt and Compose Navigation dependency and basic DI

--- app/src/main/java/de/dbaelz/demo/pnpstats/MainActivity.kt
+++ app/src/main/java/de/dbaelz/demo/pnpstats/MainActivity.kt
@@ -3,41 +3,91 @@ package de.dbaelz.demo.pnpstats
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
 import de.dbaelz.demo.pnpstats.ui.theme.PnPStatsTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PnPStatsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                val navController = rememberNavController()
+                val backstackEntry = navController.currentBackStackEntryAsState()
+                val route = backstackEntry.value?.destination?.route
+
+                Scaffold(
+                    topBar = {
+                        val title =
+                            route?.let { Screen.valueOf(it).displayName }
+                                ?: Screen.OVERVIEW.displayName
+                        TopBar(title, route != Screen.OVERVIEW.name) {
+                            navController.popBackStack()
+                        }
+                    },
+                    content = {
+                        PnPStatsNavHost(navController, Modifier.padding(it))
+                    }
+                )
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+private fun TopBar(title: String, withBackNavigation: Boolean, onBackPressed: () -> Unit) {
+    TopAppBar(
+        title = { Text(title) },
+        navigationIcon = if (withBackNavigation) {
+            {
+                IconButton(onClick = onBackPressed) {
+                    Icon(Icons.Default.ArrowBack, null)
+                }
+            }
+        } else {
+            null
+        }
+    )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    PnPStatsTheme {
-        Greeting("Android")
+private fun PnPStatsNavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.OVERVIEW.name,
+        modifier = modifier
+    ) {
+        composable(Screen.OVERVIEW.name) {
+            Text("${Screen.OVERVIEW.displayName} Screen")
+        }
+
+        composable(Screen.EXPERIENCE.name) {
+            Text("${Screen.EXPERIENCE.displayName} Screen")
+        }
+
+        composable(Screen.CURRENCY.name) {
+            Text("${Screen.CURRENCY.displayName} Screen")
+        }
     }
+}
+
+enum class Screen(val displayName: String) {
+    OVERVIEW("Overview"),
+    EXPERIENCE("Experience"),
+    CURRENCY("Currency"),
+    SETTINGS("Settings")
 }
---
+++ app/src/main/java/de/dbaelz/demo/pnpstats/PnPStatsApp.kt
@@ -0,0 +1,7 @@
+package de.dbaelz.demo.pnpstats
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PnPStatsApp : Application()
