0921d012785d7ff7092e3aea102ebf2cc972ab4d
Daniel Bälz <me@dbaelz.de>
2021-10-17 11:25:02 +0200

    Rename dark theme colors to unify naming

--- app/src/main/java/de/dbaelz/demo/pnpstats/ui/theme/Color.kt
+++ app/src/main/java/de/dbaelz/demo/pnpstats/ui/theme/Color.kt
@@ -2,10 +2,9 @@ package de.dbaelz.demo.pnpstats.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val DarkPrimary = Color(0xFFBB86FC)
+val DarkPrimaryVariant = Color(0xFF3700B3)
+val DarkSecondary = Color(0xFF03DAC5)
 
 
 val Primary = Color(0xFF64B5F6)
