082097f1a637394e3863894648d719f4c024cd16
Daniel Bälz <me@dbaelz.de>
2021-10-18 22:48:51 +0200

    Improve dummy UI of OverviewScreen

--- app/src/main/java/de/dbaelz/demo/pnpstats/ui/feature/overview/OverviewScreen.kt
+++ app/src/main/java/de/dbaelz/demo/pnpstats/ui/feature/overview/OverviewScreen.kt
@@ -1,16 +1,21 @@
 package de.dbaelz.demo.pnpstats.ui.feature.overview
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import de.dbaelz.demo.pnpstats.ui.feature.LAUNCHED_EFFECT_KEY
 import de.dbaelz.demo.pnpstats.ui.feature.common.LoadingIndicator
@@ -59,26 +64,52 @@ private fun CharacterInfo(character: de.dbaelz.demo.pnpstats.data.character.Char
         ) {
             Text(
                 text = character.name,
-                style = MaterialTheme.typography.h4,
+                style = MaterialTheme.typography.h3,
                 fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
                 modifier = Modifier.fillMaxWidth()
             )
 
-            Text(
-                text = character.experience.toString(),
-                style = MaterialTheme.typography.h5,
-                modifier = Modifier.fillMaxWidth()
+            Spacer(Modifier.height(24.dp))
+
+            CharacterDetail(Icons.Default.Info, "${character.experience} XP")
+
+            Spacer(Modifier.height(24.dp))
+
+            CharacterDetail(
+                Icons.Default.AccountBox,
+                "${character.currency.platinum} pp  • " +
+                        "${character.currency.gold} gp • " +
+                        "${character.currency.silver} sp  • " +
+                        "${character.currency.copper} cp"
             )
 
-            Text(
-                text = character.currency.toString(),
-                style = MaterialTheme.typography.h5,
-                modifier = Modifier.fillMaxWidth()
+            Spacer(Modifier.height(24.dp))
+
+            CharacterDetail(Icons.Default.Email, character.notes)
+        }
+    }
+}
+
+@Composable
+private fun CharacterDetail(icon: ImageVector, text: String) {
+    if (text.isNotEmpty()) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                imageVector = icon,
+                contentDescription = icon.name,
+                tint = MaterialTheme.colors.primary,
+                modifier = Modifier.size(48.dp)
             )
 
+            Spacer(Modifier.height(4.dp))
+
             Text(
-                text = character.notes,
+                text = text,
                 style = MaterialTheme.typography.h5,
+                textAlign = TextAlign.Center,
                 modifier = Modifier.fillMaxWidth()
             )
         }
